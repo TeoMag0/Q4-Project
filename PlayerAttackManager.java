f4121372249f1bd2693a4473563bdab9249c745c
Mateo Mac <teomag00@gmail.com>
2024-05-07 14:01:41 -0700

    ITS ALL BROKEN AAAAAĀ

--- PlayerAttackManager.java
+++ PlayerAttackManager.java
@@ -6,19 +6,44 @@ import javax.swing.event.*;
  * purpose: manage play mouse inputs
  */
 
-public class PlayerAttackManager extends Startable implements MouseInputListener{
+public class PlayerAttackManager extends Startable implements MouseInputListener, Runnable{
     private Player player;
+    private boolean active;
+    private Vector2 mouseWorldPosition;
+    private boolean mouseDown;
+    private float firerate;
+    private Thread curFiringThread;
 
     public PlayerAttackManager(Player player){
         this.player = player;
+        active = true;
+        mouseDown = false;
+        firerate = 1;
     }
     public void start(){
         Screen.Singleton.addMouseListener(this);
         Screen.Singleton.addMouseMotionListener(this);
     }
 
-    public void mouseDragged(MouseEvent e){
+    public void run(){
+        try{
+            while(mouseDown){
+                if(active){
+                    launchProjectile();
+                }
+                Thread.sleep((int)(1000f/firerate));
+            }
+        }catch(InterruptedException e){
 
+        }
+    }
+    public void launchProjectile(){
+        float speed = 0;
+        new PlayerProjectile("Parentheses.png", player.getPos(), 1f, Vector2.multiply(Vector2.difference(mouseWorldPosition, player.getPos()).normalized(), speed));
+    }
+
+    public void mouseDragged(MouseEvent e){
+        mouseWorldPosition = Screen.getWorldCoords(new Vector2(e.getX(), e.getY()));
     }
     public void mouseMoved(MouseEvent e){
         
@@ -27,7 +52,10 @@ public class PlayerAttackManager extends Startable implements MouseInputListener
 
     }
     public void mousePressed(MouseEvent e) {
-
+        mouseDown = true;
+        mouseWorldPosition = Screen.getWorldCoords(new Vector2(e.getX(), e.getY()));
+        curFiringThread = new Thread(this);
+        curFiringThread.start();
     }
     
     public void mouseReleased(MouseEvent e) {
@@ -36,7 +64,7 @@ public class PlayerAttackManager extends Startable implements MouseInputListener
         if(e.getButton() == MouseEvent.BUTTON1){
             //TileMap.Singleton.addTile(coords, TilePic.STONE_BRICK_FLOOR, false);
             //TileMap.Singleton.saveMap();
-            System.out.println(TileMap.Singleton.coordsToRC(coords));
+            //System.out.println(TileMap.Singleton.coordsToRC(coords));
         }else if(e.getButton() == MouseEvent.BUTTON3) {
             TileMap.Singleton.addTile(coords, TilePic.JAIL_BARS, true);
             TileMap.Singleton.saveMap();
@@ -44,6 +72,7 @@ public class PlayerAttackManager extends Startable implements MouseInputListener
             TileMap.Singleton.removeTile(coords);
             TileMap.Singleton.saveMap();
         }
+        mouseDown = false;
     }
     
 
@@ -54,5 +83,9 @@ public class PlayerAttackManager extends Startable implements MouseInputListener
     
     public void mouseExited(MouseEvent e) {
 
+    }
+
+    public void setActive(boolean active){
+        this.active = active;
     }
 }
---
+++ PlayerProjectile.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+/*
+ * purpose: player's projectile
+ * to update: make width of collider line up with picture
+ *              and if penetrates is false send to server, and send active state to server
+ */
+
+public class PlayerProjectile extends Projectile{
+    private BufferedImage pic;
+    private Vector2 size;
+    private BoxCollider collider;
+
+    public PlayerProjectile(String pic, Vector2 position, float size, Vector2 velocity){
+        super(position, velocity);
+        this.size = new Vector2(size, size);
+
+        //get the x size
+        collider = new BoxCollider(this, new Vector2(size, size), ColliderPurpose.PLAYER_PROJECTILE);
+
+        try{
+            this.pic = ImageIO.read(new File(pic));
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public void drawMe(Graphics g){
+        Vector2 drawPoint = Screen.getScreenCoords(Vector2.sum(getPos(), new Vector2(-size.getY()/2, size.getY()/2)));
+        g.drawImage(pic, drawPoint.intX(), drawPoint.intY(), Screen.toPixels(size.intX()), Screen.toPixels(size.intY()), null);
+        System.out.println("drawn");
+    }
+
+    public void onCollisionEnter(Collider col){
+        if(col.purpose() == ColliderPurpose.WALL){
+            destroySelf();
+        }
+    }
+    public void destroySelf(){
+        allProjectiles.remove(this);
+        Collider.colliderList().remove(collider);
+    }
+}
